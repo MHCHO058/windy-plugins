07150786330779b1ed46f88ae88cca68dbd344a4
MHCHO058 <34257964+MHCHO058@users.noreply.github.com>
2019-01-14 21:28:08 +0900

    testupload

--- src/config.js
+++ src/config.js
@@ -1,11 +1,30 @@
 module.exports = {
 
 	// Display name of your plugin, as it will appear in the menu
-	displayName: 'My super plugin',
+	displayName: 'Hermesys_Test',
+
 
 	// Place, where opening link to your plugin will appear. So far only
 	// Allowed: 'contextmenu', 'menu'
 	hook: 'menu',
+	/* hook: 'contextmenu', */
+
+	className: 'plugin-rhpane plugin-mobile-rhpane',
+	
+	/* dependencies: [
+		'https://unpkg.com/d3@5.7.0/dist/d3.min.js'
+	],
+	
+	
+	className: 'drop-down-menu',
+	classNameMobile: 'drop-down-menu down', // for mobile devices
+	attachPoint: '#map_container .leaflet-popup-pane',
+	attachPointMobile: '#plugins' // for mobile devices
+	 */
+	
+	
+	// className: 'plugin-rhpane plugin-mobile-rhpane',
+	exclusive: 'rhpane',
 
 	// List of external libraries, taht should be loade before
 	// your plugin is even mounted to the page
